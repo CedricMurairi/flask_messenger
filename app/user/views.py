171e1cfea4709a0d5a93030ed159249d150539d2
Cedric Murairi <murairicedric@gmail.com>
2020-07-27 20:38:11 +0200

    handle serve side of different request with dummy json return for channel creation, join and start conversation

--- app/user/views.py
+++ app/user/views.py
@@ -9,6 +9,7 @@ from .. import db
 
 @user.route("/create-channel", methods=['GET', 'POST'])
 def create_channel():
+
 	if request.method == 'POST':
 		print('Hey there, here is the post from create-channel route')
 		channel_name = request.form.get('channel_name')
@@ -16,30 +17,48 @@ def create_channel():
 		username = session.get('username')
 		email = session.get('email')
 		channel_creator = User.query.filter_by(username=username, email=email).first()
+
 		if(channel_creator):
+			check_channel = Channel.query.filter_by(name=channel_name).first()
+
+			if(check_channel):
+				return jsonify({'response': 'Channel already in our records'})
+
 			new_channel = Channel(name=channel_name, description=channel_description, channel_creator=channel_creator.id)
 			db.session.add(new_channel)
 			db.session.commit()
-		return jsonify({'response': 'Post content recieved'})
+			return jsonify({'response': 'Channel created successfully'})
+
 	return redirect(url_for('index'))
 
 
 @user.route("/join-channel", methods=['GET', 'POST'])
 def join_channel():
+
 	if request.method == 'POST':
 		print('Hey there, here is the post from join-channel route')
 		channel_name = request.form.get('search_channel')
-		channels = Channel.query,filter_by(name=channel_name).first()
-		if(channels):
-			for channel in channels:
-				print(channel.description)
-		return jsonify({'response': 'Post content recieved'})
+		channel = Channel.query.filter_by(name=channel_name).first()
+
+		if(channel):
+			print(channel.description)
+			return jsonify({'response': 'Channel does exist let\'s check it out'})
+
+		return jsonify({'response': 'No such a channel in our records'})
+
 	return redirect(url_for('index'))
 
 
 @user.route("/conversation", methods=['GET', 'POST'])
 def conversation():
+
 	if request.method == 'POST':
-		print('Hey there, here is the post from conversation route')
-		return jsonify({'response': 'Post content recieved'})
+		search_user_name = request.form.get('search_people')
+		user = User.query.filter_by(username=search_user_name).all()
+
+		if(user):
+			return jsonify({'response': [user.username for user in user]})
+
+		return jsonify({'response': 'No such a user in our records'})
+
 	return redirect(url_for('index'))
