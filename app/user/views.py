173096211b4a0ab9eb28c9536bda761c47a1d97b
Cedric Murairi <murairicedric@gmail.com>
2020-08-21 23:14:49 +0200

    added two more views to handle the join channel and connect to user

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 from flask import request, jsonify, render_template ,redirect, url_for, session
-
-from ..models import User, Channel
+from ..models import User, Channel, Connection
 from . import user
 from .. import db
 
@@ -32,9 +31,8 @@ def create_channel():
 	return redirect(url_for('index'))
 
 
-@user.route("/join-channel", methods=['GET', 'POST'])
-def join_channel():
-
+@user.route("/join-channel/channels", methods=['GET', 'POST'])
+def search_channel():
 	if request.method == 'POST':
 		print('Hey there, here is the post from join-channel route')
 		channel_name = request.form.get('search_channel')
@@ -42,23 +40,65 @@ def join_channel():
 
 		if(channel):
 			print(channel.description)
-			return jsonify({'response': 'Channel does exist let\'s check it out'})
+			return jsonify({'response': True, 'data': {'id': channel.id, 'name': channel.name, 'description': channel.description, 'created': channel.channel_creation, 'creator': channel.channel_creator}})
 
 		return jsonify({'response': 'No such a channel in our records'})
 
 	return redirect(url_for('index'))
 
 
-@user.route("/conversation", methods=['GET', 'POST'])
-def conversation():
+@user.route("/join-channel/channels/join", methods=['POST'])
+def join_channel():
+	print('Hey there, here is the post from join-channel route')
+	channel_id = request.form.get('channel_id')
+	channel = Channel.query.filter_by(id=channel_id).first()
+
+	if(channel):
+		username = session.get('username')
+		email = session.get('email')
+		user = User.query.filter_by(username=username, email=email).first()
+		user.joined_channel = channel.id
+		db.session.add(user)
+		db.session.commit()
+		return jsonify({'response': 'Channel joined successfully', 'data': {'name': channel.name, 'description': channel.description, 'created': channel.channel_creation, 'creator': channel.channel_creator}})
+
+
+@user.route("/conversation/user", methods=['GET', 'POST'])
+def search_user():
 
 	if request.method == 'POST':
 		search_user_name = request.form.get('search_people')
 		user = User.query.filter_by(username=search_user_name).all()
 
 		if(user):
-			return jsonify({'response': [user.username for user in user]})
+			return jsonify({'response': [{'username': user.username, 'email': user.email, 'joined': user.joined, 'is_active': user.is_active, 'last_seen': user.last_seen} for user in user]})
 
 		return jsonify({'response': 'No such a user in our records'})
 
 	return redirect(url_for('index'))
+
+
+@user.route("/conversation/user/join", methods=['POST'])
+def join_conversation():
+
+	user_connection = request.form.get('user_id')
+	if(user_connection):
+		username = session.get('username')
+		email = session.get('email')
+		user = User.query.filter_by(username=username, email=email).first()
+		connection = Connection(from_id=user.id, to_id=user_connection)
+		db.session.add(connection)
+		db.session.commit()
+		return jsonify({'response': 'Connection created successfully'})
+
+
+@user.route("/<string:username>/settings", methods=['GET', 'POST'])
+def settings(username):
+
+	return '<h2>Hello %s, here is your setting</h2>' %username
+
+
+@user.route("/<string:username>/profile", methods=['GET', 'POST'])
+def profile(username):
+
+	return '<h2>Hello %s, here is your profile</h2>'  %username
