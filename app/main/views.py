590f226aaf19bbbadcd37d211e5ca7c7302fcb06
Cedric Murairi <murairicedric@gmail.com>
2020-08-30 14:21:31 +0200

    added a before app request to ping the user and update his last seen

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,19 @@
 from . import main
+from datetime import datetime
 from flask import render_template, url_for, request, session, jsonify, flash, redirect
 from ..models import User, Channel, Connection
+from .. import db
 from sqlalchemy import or_
+
+@main.before_app_request
+def ping_user():
+	username = session.get('username')
+	email = session.get('email')
+	user = User.query.filter_by(username=username, email=email).first()
+	if user:
+		user.last_seen = datetime.utcnow()
+		db.session.add(user)
+		db.session.commit()
 
 
 @main.route('/', methods=['GET'])
@@ -18,21 +30,21 @@ def index():
 
 		for connection in connections:
 			if (connection.from_id == user.id):
-				conversation_user.push(connection.to_id)
+				conversation_user_id.append(connection.to_id)
 			if (connection.to_id == user.id):
-				conversation_user.push(connection.from_id)
+				conversation_user_id.append(connection.from_id)
 
-		conversation_user = User.query.filter(User.id.in_(conversation_user_id)).all()
+		conversation_users = User.query.filter(User.id.in_(conversation_user_id)).all()
 
-		if channel_created:
-			for channel in channel_created:
-				print(channel.name, channel.description)
-		if channel_joined:
-			for channel in channel_joined:
-				print(channel.name, channel.description)
-		else:
-			print('There was nothing in joined channels')
+		# if channel_created:
+		# 	for channel in channel_created:
+		# 		print(channel.name, channel.description)
+		# if channel_joined:
+		# 	for channel in channel_joined:
+		# 		print(channel.name, channel.description)
+		# else:
+		# 	print('There was nothing in joined channels')
 		if user:
 			flash('Already logged in')
-			return render_template('index.html',conversation_user=conversation_user, channel_joined=channel_joined, channel_created=channel_created, user=user)
+			return render_template('index.html',conversation_users=conversation_users, channel_joined=channel_joined, channel_created=channel_created, user=user)
 	return redirect(url_for('auth.login'))
