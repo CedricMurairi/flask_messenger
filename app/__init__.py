0dfe03b3a774b85e6dc757fac722d82d600e5b26
Cedric Murairi <murairicedric@gmail.com>
2020-08-30 14:27:16 +0200

    modified __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from config import config
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
+moment = Moment()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -11,6 +15,8 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     db.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
 
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
