f4169225b25fa0ed7335de65e5e1b657e49717b4
Cedric Murairi <murairicedric@gmail.com>
2020-09-18 22:55:43 +0200

    fix index file

--- app/main/views.py
+++ app/main/views.py
@@ -3,33 +3,40 @@ from datetime import datetime
 from flask import render_template, url_for, request, session, jsonify, flash, redirect
 from ..models import User, Channel, Connection
 from .. import db
+from flask_login import current_user
 from sqlalchemy import or_
 
 @main.before_app_request
 def ping_user():
-	username = session.get('username')
-	email = session.get('email')
-	user = User.query.filter_by(username=username, email=email).first()
-	if user:
-		user.last_seen = datetime.utcnow()
-		db.session.add(user)
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.utcnow()
+		db.session.add(current_user)
 		db.session.commit()
+	# if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth':
+	# 	return redirect(url_for('auth.unconfirmed'))
 
 
 @main.route('/', methods=['GET'])
 def index():
-	channel_created = current_user.channels
-	channel_joined = current_user.joined_channels
-	connections = Connection.query.filter(or_(Connection.from_id==user.id, Connection.to_id==user.id)).all()
+	if current_user.is_anonymous:
+		channel_created = None
+		channel_joined = None
+		connections = None
+		conversation_users = None
 
-	conversation_user_id = []
+	if current_user.is_authenticated:
+		channel_created = current_user.channels
+		channel_joined = current_user.joined_channels
+		connections = Connection.query.filter(or_(Connection.from_id==current_user.id, Connection.to_id==current_user.id)).all()
 
-    for connection in connections:
-		if (connection.from_id == user.id):
-			conversation_user_id.append(connection.to_id)
-		if (connection.to_id == user.id):
-			conversation_user_id.append(connection.from_id)
+		conversation_user_id = []
 
-	conversation_users = User.query.filter(User.id.in_(conversation_user_id)).all()
+		for connection in connections:
+			if (connection.from_id == user.id):
+				conversation_user_id.append(connection.to_id)
+			if (connection.to_id == user.id):
+				conversation_user_id.append(connection.from_id)
 
-	return render_template('index.html',conversation_users=conversation_users, channel_joined=channel_joined, channel_created=channel_created, user=user)
+		conversation_users = User.query.filter(User.id.in_(conversation_user_id)).all()
+
+	return render_template('index.html',conversation_users=conversation_users, channel_joined=channel_joined, channel_created=channel_created)
