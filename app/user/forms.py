afeab8ee50be9eb440339993416ac80efdbefea0
Cedric Murairi <murairicedric@gmail.com>
2020-09-24 18:12:33 +0200

    added user level form managment to handle channel creation, search channel and user search

--- app/user/forms.py
+++ app/user/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, ValidationError, TextAreaField
-from wtforms.validators import InputRequired, EqualTo, Length, Regexp, NumberRange, Email
+from wtforms import StringField, SubmitField, ValidationError, TextAreaField
+from wtforms.validators import InputRequired, Email
 from email_validator import validate_email
-from ..models import User
+from ..models import Channel
 
 class CreateChannelForm(FlaskForm):
 	name = StringField('Channel name', validators=[InputRequired()])
@@ -11,16 +11,16 @@ class CreateChannelForm(FlaskForm):
 
 	def validate_name(self):
 		if Channel.query.filter_by(name=name).first():
-			aise ValidationError('Channel name already in use')
+			raise ValidationError('Channel name already in use')
 
 
 class SearchUserForm(FlaskForm):
-	username = StringField('Username')
-	email = StringField('Email')
+	username = StringField('Username', validators=[InputRequired()])
+	email = StringField('Email', validators=[Email()])
 	submit = SubmitField('Search')
 
 
 class SearchChannelForm(FlaskForm):
 	name = StringField('Channel name', validators=[InputRequired()])
-	description = TextAreaField('Channel description', validators=[InputRequired()])
+	description = TextAreaField('Channel description')
 	submit = SubmitField('Search')
